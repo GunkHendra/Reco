06970345bf2274dab8601fd2a85d75619ecb1ac8
Gung Hendra <gunghencrypt@gmail.com>
2025-05-02 01:42:31 +0800

    fix build error

--- src/app/chat/[chat_id]/page.tsx
+++ src/app/chat/[chat_id]/page.tsx
@@ -1,14 +1,13 @@
-
-const Chat = ({ params }: { params: { chat_id: string } }) => {
+const Chat = async ({ params }: { params: { chat_id: string } }) => {
   const { chat_id } = params;
 
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">This is the Chat Page of Chat {chat_id}</h2>
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        This is the Chat Page of Chat {chat_id}
+      </h2>
+    </div>
   );
-}
+};
 
 export default Chat;
